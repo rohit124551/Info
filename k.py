90c9617e38361c5114533613312b2ccd87730d2f
Rohit Kumar Ranjan <rohit124551@gmail.com>
2024-02-22 15:45:34 +0530

    srs page added

---
+++ k.py
@@ -0,0 +1,26 @@
+import os
+
+folder_path = 'E:\Info\Info\ALL SRS'  # Replace this with the path to your folder
+
+html_content = '''<html>
+<head>
+  <title>PDF Files</title>
+</head>
+<body>
+'''
+
+for foldername, subfolders, filenames in os.walk(folder_path):
+    for filename in filenames:
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(foldername, filename)
+            link_text = os.path.basename(filename)
+            link_href = os.path.relpath(file_path, folder_path).replace('\\', '/')
+
+            html_content += f'''  <a class="link" href="ALL SRS\{link_href}">{link_text}</a><br>'''
+
+html_content += '</body></html>'
+
+with open('output.html', 'w') as html_file:
+    html_file.write(html_content)
+
+print("HTML file created successfully!")
